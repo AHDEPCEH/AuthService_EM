5c4ef2c4f79881732ebc5e1b11fee4a14dbb8fcb
AHDEPCEH <114348208+AHDEPCEH@users.noreply.github.com>
2026-02-18 12:07:57 +0500

    add models in user app

--- AuthService/AuthService/settings.py
+++ AuthService/AuthService/settings.py
@@ -1,8 +1,8 @@
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-fr_y)rm4k!iksf97k$7&fjmy-*)_fh9@n^n0_v!a^v!9ivkw+n'
@@ -12,24 +12,19 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
@@ -44,7 +39,6 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
@@ -53,9 +47,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'AuthService.wsgi.application'
 
-
 # Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -64,24 +56,7 @@ DATABASES = {
     }
 }
 
-
-# Password validation
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
+AUTH_PASSWORD_VALIDATORS = []
 
 # Internationalization
 
@@ -93,7 +68,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = 'static/'
+
+JWT_SETTINGS = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=24),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ALGORITHM': 'HS256',
+}
--- AuthService/user/models.py
+++ AuthService/user/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.db.models import CharField, BooleanField, DateTimeField, ForeignKey
 
-# Create your models here.
+
+class Role(models.Model):
+    name = CharField(max_length=25, unique=True)
+
+    class Meta:
+        db_table = "roles"
+
+    def __str__(self):
+        return self.name
+
+
+class Permission(models.Model):
+    resource = CharField(max_length=50)
+    action = CharField(max_length=50)
+
+    class Meta:
+        db_table = "permissions"
+        unique_together = ["resource", "action"]
+
+    def __str__(self):
+        return f"{self.resource}:{self.action}"
+
+
+class PermissionRole(models.Model):
+    role_id = ForeignKey(Role, on_delete=models.CASCADE)
+    permission_id = ForeignKey(Permission, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "permission_roles"
+        unique_together = ["role_id", "permission_id"]
+
+
+class User(models.Model):
+    email = CharField(unique=True)
+    password = CharField(max_length=128)
+    name = CharField(max_length=50)
+    is_active = BooleanField(default=True)
+    role_id = ForeignKey(Role, on_delete=models.SET_NULL, null=True)
+    created_at = DateTimeField(auto_now_add=True)
+    updated_at = DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "users"
+
+    def __str__(self):
+        return f"{self.name}, with email: {self.email}"
